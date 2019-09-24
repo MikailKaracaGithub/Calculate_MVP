c60c3b94bf20dbc24ff157ab12fa90bbe7672152
Xavaltir <tombis1999@hotmail.com>
2019-09-25 00:35:01 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_MVP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int avatar1BaseHP = 100;
+            int avatar1capeHP = 40;
+            int ringHP = 50;
+
+            int totalBaseHP = avatar1BaseHP;
+            int totalArmourHP = avatar1capeHP + ringHP;
+            int totalHP = totalBaseHP + totalArmourHP;
+
+            //Situation 1
+
+            Console.WriteLine("Avatar 1, situation 1: " + totalHP);
+
+            //Situation 2
+
+            int gauntletHP = 33;
+            totalArmourHP += gauntletHP;
+            totalHP = totalBaseHP + totalArmourHP;
+            Console.WriteLine("Avatar 1, situation 1: " + totalHP);
+
+            //Situation 3
+
+            totalBaseHP *= 2;
+            totalHP = totalBaseHP + totalArmourHP;
+            Console.WriteLine("Avatar 1, situation 1: " + totalHP);
+
+            //Situation 4
+
+            totalArmourHP *= 3;
+            totalHP = totalBaseHP + totalArmourHP;
+            Console.WriteLine("Avatar 1, situation 1: " + totalHP);
+
+            Console.ReadLine();
+        }
+    }
+}
